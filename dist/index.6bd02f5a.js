4d49315a884641e39d35467e84a8fbed22bc6dc4
DBugghi <dibugghi@gmail.com>
2024-04-25 13:55:14 +0530

    production ready

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,19 @@
+/**
+ * Header
+ * --Logo
+ * --Nav Items
+ * Body
+ * --Search
+ * --RestroContainer
+ *   --RestoCards
+ * Footer
+ * --Disclaimer
+ * --Links, Address, Contact
+ */ const AppLayout = ()=>{
+    return div.app;
+};
+_c = AppLayout;
+var _c;
+$RefreshReg$(_c, "AppLayout");
+
+//# sourceMappingURL=index.6bd02f5a.js.map
