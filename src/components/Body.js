16757a91a3dec266c4a7ca51c7898a6ea9443eb3
DBugghi <dbugghi@gmail.com>
2024-06-05 17:17:04 +0530

    top rated

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,36 @@
 import RestrauntCard from "./RestrauntCard";
-import mockData from "../utils/mockdata";
+import mockData from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    //Local state variable - Super powerful variable
+    const [listOfRestuarants, setListOfRestaurant] = useState(mockData);
+    const [above45, setAbove45] = useState(false);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        if(!above45){                           
+                            const filteredList = listOfRestuarants.filter(
+                                (res) => res.info.avgRating > 4.5
+                            );
+                            setListOfRestaurant(filteredList);
+                            setAbove45(true);
+                        } 
+                        else{
+                            setListOfRestaurant(mockData); 
+                            setAbove45(false);
+                        }
+                    }}    
+                >
+                    {!above45 ? "Top Rated Restuarants" : "Show All"}
+                </button>
+            </div>
             <div className="res-container">
-                {mockData.map(restraunt => <RestrauntCard key={restraunt.info.id} resData={restraunt}/>)}
+                {listOfRestuarants.map((restaurant) => <RestrauntCard key={restaurant.info.id} resData={restaurant}/>)}
             </div>
         </div>
     )
