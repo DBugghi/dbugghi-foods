3a9e17e880271e0b0df44a76f79411ce432ba9f1
DBugghi <dibugghi@gmail.com>
2024-06-17 15:32:21 +0530

    Live API + css fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import RestrauntCard from "./RestrauntCard";
-import mockData from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
     //Local state variable - Super powerful variable
-    const [listOfRestuarants, setListOfRestaurant] = useState(mockData);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [apiWaleRestaurants, setApiWaleRestaurants] = useState([]);
     const [above45, setAbove45] = useState(false);
 
+    useEffect(()=>{
+        fetchData();
+    }, []);
+
+    const fetchData = async () =>{
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2418194&lng=72.49297159999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        
+        const json = await data.json();
+        setApiWaleRestaurants(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurants(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    }
     return (
         <div className="body">
             <div className="filter">
@@ -14,14 +27,14 @@ const Body = () => {
                     className="filter-btn"
                     onClick={() => {
                         if(!above45){                           
-                            const filteredList = listOfRestuarants.filter(
+                            const filteredList = listOfRestaurants.filter(
                                 (res) => res.info.avgRating > 4.5
                             );
-                            setListOfRestaurant(filteredList);
+                            setListOfRestaurants(filteredList);
                             setAbove45(true);
                         } 
                         else{
-                            setListOfRestaurant(mockData); 
+                            setListOfRestaurants(apiWaleRestaurants); 
                             setAbove45(false);
                         }
                     }}    
@@ -30,7 +43,7 @@ const Body = () => {
                 </button>
             </div>
             <div className="res-container">
-                {listOfRestuarants.map((restaurant) => <RestrauntCard key={restaurant.info.id} resData={restaurant}/>)}
+                {listOfRestaurants.map((restaurant) => <RestrauntCard key={restaurant.info.id} resData={restaurant}/>)}
             </div>
         </div>
     )
